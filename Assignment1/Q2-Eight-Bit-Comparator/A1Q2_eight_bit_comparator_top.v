25a11adc75f6dc20ba52e52b044b9751df766736
server-crash <shubhanr20@iitk.ac.in>
2022-01-24 02:44:04 +0530

    harshit

--- Assignment1/Q2-Eight-Bit-Comparator/A1Q2_eight_bit_comparator_top.v
+++ Assignment1/Q2-Eight-Bit-Comparator/A1Q2_eight_bit_comparator_top.v
@@ -1,17 +1,16 @@
 `include "A1Q2_eight_bit_comparator.v"
-module test_bench();
+module eight_bit_comparator_top();
     wire l,e,g;
     reg[7:0] a,b;
     eight_bit_comparator comp(l, e, g, a, b);
-    initial
-    begin
+
+    initial begin
         $display("time ||   a |   b || lesser | equal | greater ");
         $monitor("%4d || %d | %d || %6d | %5d | %7d", $time,a,b,l,e,g);
         a = 45; b=45;
         repeat(15) begin
           #5
-          a = $urandom%256;
-          b = $urandom%256;
+          a = $urandom%256; b = $urandom%256;
         end
         $finish;
     end

762a1e8868360464930ff7db1cdf7e5dcb43357b
Harshit Raj <harshitr20@iitk.ac.in>
2022-01-24 02:38:52 +0530

    Add cin

--- Assignment1/Q1-Eight-Bit-Ripple-Carry-Adder/A1Q1_eight_bit_adder_top.v
+++ Assignment1/Q1-Eight-Bit-Ripple-Carry-Adder/A1Q1_eight_bit_adder_top.v
@@ -12,8 +12,9 @@ module eight_bit_adder_top ();
 	eight_bit_ripple_carry_adder EBA (sum, cout, a, b, cin);
 
 	initial begin
-		$monitor("Time: %4d, %d + %d = %d, cout: %d", $time, a, b, sum, cout);
-		repeat(20) begin
+		// We've also tested by varrying cin to properly test the module
+		$monitor("Time: %4d, %d + %d + %d = %d, cout: %d", $time, a, b, cin, sum, cout);
+		repeat(15) begin
 			#5
 			a = $random % 256; b = $random % 256; cin = $random % 2;
 		end

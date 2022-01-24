7e0bd8789acb0070034d4cb34f0b7b5035fd48f4
Harshit Raj <75195728+1-Harshit@users.noreply.github.com>
2022-01-24 15:24:40 +0530

    Fix formatting

--- Assignment1/Q3-2-to-4-Decoder/A1Q3_two_to_four_decoder.v
+++ Assignment1/Q3-2-to-4-Decoder/A1Q3_two_to_four_decoder.v
@@ -2,6 +2,7 @@ module two_to_four_decoder(op,inp,enable);
     output [3:0] op;
     input [1:0] inp;
     input enable;
+    
     assign op[0]=~inp[0]&&~inp[1]&&enable;
     assign op[1]=inp[0]&&~inp[1]&&enable;
     assign op[2]=~inp[0]&&inp[1]&&enable;
--- Assignment1/Q3-2-to-4-Decoder/A1Q3_two_to_four_decoder_test.v
+++ Assignment1/Q3-2-to-4-Decoder/A1Q3_two_to_four_decoder_test.v
@@ -1,16 +1,19 @@
 `include "A1Q3_three_to_eight_decoder.v"
+
 module two_to_four_decoder_test();
     reg[2:0] INP=3'd0;
     wire [7:0] OP;
+    
     three_to_eight_decoder d0(OP,INP,1'b1);
+    
     initial begin
         $monitor("%b",OP);
-        #5INP=3'd1;
-        #5INP=3'd2;
-        #5INP=3'd3;
-        #5INP=3'd4;
-        #5INP=3'd5;
-        #5INP=3'd6;
-        #5INP=3'd7;
+        #5 INP=3'd1;
+        #5 INP=3'd2;
+        #5 INP=3'd3;
+        #5 INP=3'd4;
+        #5 INP=3'd5;
+        #5 INP=3'd6;
+        #5 INP=3'd7;
     end
 endmodule

8476ade6bf74d541e75b277a9b8f589e03661e62
server-crash <shubhanr20@iitk.ac.in>
2022-01-24 19:39:23 +0530

    add comments

--- Assignment1/Q2-Eight-Bit-Comparator/A1Q2_one_bit_comparator.v
+++ Assignment1/Q2-Eight-Bit-Comparator/A1Q2_one_bit_comparator.v
@@ -1,6 +1,6 @@
 module one_bit_comparator(out_less, out_equal, out_more, a, b, in_less, in_equal, in_more);
-    input a,b,in_less,in_equal,in_more;
-    output out_less,out_equal,out_more;
+    input a,b,in_less,in_equal,in_more; //in_less, in_equal, in_more are from inputs from previous one_bit_comparator
+    output out_less,out_equal,out_more; //outputs
 
     assign out_equal=((a&&b)||(~a&&~b))&&in_equal;
     assign out_less=(~a&&b||in_less)&&~in_more;

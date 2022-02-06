9543fa5a250b520a773df1f5cf5e2cd3c62a850d
server-crash <shubhanr20@iitk.ac.in>
2022-02-06 22:30:48 +0530

    finished q1

--- Assignment2/Q2-Johnson_Counter/johnson_counter.v
+++ Assignment2/Q2-Johnson_Counter/johnson_counter.v
@@ -3,9 +3,9 @@ module johnson_counter(q,clk);
     input clk;
     output [0:7] q; //wrote [0:7] so that in test bench,
     //it is printed in the order according to the truth table given in report
-    d_flip_flop dff1(.q(q[0]), .clk(clk), .d(~q[7])); //implementing the circuit according to circuit
-    d_flip_flop dff2(.q(q[1]), .clk(clk), .d(q[0])); //diagram in report
-    d_flip_flop dff3(.q(q[2]), .clk(clk), .d(q[1]));
+    d_flip_flop dff1(.q(q[0]), .clk(clk), .d(~q[7])); 
+    d_flip_flop dff2(.q(q[1]), .clk(clk), .d(q[0])); //implementing the circuit according to circuit
+    d_flip_flop dff3(.q(q[2]), .clk(clk), .d(q[1])); //diagram in report
     d_flip_flop dff4(.q(q[3]), .clk(clk), .d(q[2]));
     d_flip_flop dff5(.q(q[4]), .clk(clk), .d(q[3]));
     d_flip_flop dff6(.q(q[5]), .clk(clk), .d(q[4]));

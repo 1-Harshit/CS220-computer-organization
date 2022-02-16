4ac9121216aef0e98b6cc151e0903b1e797d848b
server-crash <shubhanr20@iitk.ac.in>
2022-02-17 01:17:08 +0530

    works

--- Assignment3/Q1-sequence-detector/A3Q1_sequence_detector.v
+++ Assignment3/Q1-sequence-detector/A3Q1_sequence_detector.v
@@ -1,47 +1,11 @@
 `include "A3Q1_d_flip_flop.v"
-module sequence_detector (outp, clk, seq, q, i, d, out);
-
-parameter SEQUENCE=4'b0101;
-parameter INPUT_WIDTH=8;
-
-input clk;
-input [INPUT_WIDTH-1:0] seq;
-output reg [INPUT_WIDTH-1:0] out;
-output reg [INPUT_WIDTH-1:0] outp;
-
-output wire [3:0] q;
-output reg [3:0] i;
-output reg d;
-
-d_flip_flop DFF1 (q[3], d, clk);
-d_flip_flop DFF2 (q[2], q[3], clk);
-d_flip_flop DFF3 (q[1], q[2], clk);
-d_flip_flop DFF4 (q[0], q[1], clk);
-
-initial begin
-	i=0;
-end
-
-always @(posedge clk) begin
-	if(i<4) begin
-		d<=seq[i];
-	end
-	else if(i<INPUT_WIDTH) begin
-		if(q==4'b0101)
-			out[i-4] <= 1;
-		else
-			out[i-4] <= 0;
-		d <= seq[i];
-		
-	end
-	else if(i<INPUT_WIDTH+4) begin
-		out[i-4] = 0;
-	end
-	else begin 
-		outp = out;
-		$finish;
-	end
-	i=i+1;
-end
-	
+module sequence_detector (out,k,inp,clk);
+  output out;
+  input inp;
+  input clk;
+  output wire[2:0] k; //internal wire
+  d_flip_flop D1(k[0],inp,clk);
+  d_flip_flop D2(k[1],k[0],clk);
+  d_flip_flop D3(k[2],k[1],clk);
+  assign out = inp & ~k[0] &k[1]&~k[2];
 endmodule
--- Assignment3/Q1-sequence-detector/A3Q1_sequence_detector_tb.v
+++ Assignment3/Q1-sequence-detector/A3Q1_sequence_detector_tb.v
@@ -1,21 +1,34 @@
 `include "A3Q1_sequence_detector.v"
 module sequence_detector_tb();
-	reg clk;
-	wire [7:0] out, o;
-	reg [7:0] seq;
-	wire [3:0] q, i;
-	wire d;
-
-	sequence_detector sd (out, clk, seq, q, i, d, o);
-	
+	reg [7:0] out=8'b00000000;
+	reg [7:0] seq=8'b01010101;
+	reg i=0;
+	wire o;
+	wire clk;
+	integer count=7;
+	wire[2:0] deb;
+	clock_module C1(clk);
+	sequence_detector sd (o,deb,i, clk);
 	initial begin
-		clk = 0;
-		seq=8'b01101010;
-		$monitor("out: %b | seq: %b | %b %d %b %b", out, seq, q, i, d, o);
-		repeat(500) begin
-			#1 clk = ~clk;
-		end
-		$finish;
+		// $monitor("%b %b %b",deb,i,o);
 	end
-	
+	always @(negedge clk) begin
+		i=seq[count];
+		#1
+		out[count-1]=o;
+		count=count-1;
+		if(count==-1) begin
+			$display("%b %b", out, seq);
+		  count=7;
+		end
+	end
+endmodule
+
+module clock_module(clk);
+	output reg clk=1'b1;
+	initial begin
+		repeat(17) begin
+			#3 clk=~clk;
+		end
+	end
 endmodule
